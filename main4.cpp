ed21fd695987d5cc897e05c7331d4e5ce14dd4a6
ABDALLAH MOHAMMED ALMHDY <abdallahmohamedalmhdy@gmail.com>
2024-12-23 23:04:17 -1000

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,392 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,i,Average,Sum;
+    cout<<"Enter the studant marrks"<<endl;
+    for(i=0; i<10; i++){
+        cin>>x;
+}
+if(x>1){
+    cout<<"Positive"<<endl;
+}
+else if(x<1){
+    cout<<"negative"<<endl;
+}
+Average=x+x+x+x+x+x+x+x+x+x/10;
+Sum=x+x+x+x+x+x+x+x+x+x;
+cout<<"The SUM is="<<Sum<<endl;
+cout<<"The average is="<<Average<<endl;*/
+
+int a[10];
+int Average,Total;
+cout<<"Enter the studant marks"<<endl;
+cin>>a[0];
+cin>>a[1];
+cin>>a[2];
+cin>>a[3];
+cin>>a[4];
+cin>>a[5];
+cin>>a[6];
+cin>>a[7];
+cin>>a[8];
+cin>>a[9];
+Total =a+a+a+a+a+a+a+a+a+a;
+Average =Total/10;
+cout<<"The total is="<<Total<<endl;
+cout<<"The Average is="<< Average<<endl;*/
+int x[10],i;
+int Total,Average;
+cout<<"Enter the number of studant"<<endl;
+for(i=0; i<10; i++){
+    cin>>x[i];
+}
+Total =Total+=x[i];
+Average =Total/10;
+cout<<"The total is ="<<Total<<endl;
+cout<<"The Average is ="<<Average<<endl;
+if(Average<0){
+cout<<"Positive"<<endl;
+}else if(Average>0){
+cout<<"negative"<<endl;*/
+
+int a[3][3];
+int b[3][3];
+int c[3][3];
+int i,j;
+cout<<"Enter the values of a"<<endl;
+for(i=0 ;i<3; i++){
+        cout<<endl;
+    for(j=0; j<3 ; j++){
+}
+}
+ cin >>a[i][j];
+cout<<"Enter the values of b"<<endl;
+for(i=0; i<3; i++){
+        cout<<endl;
+    for(j=0; j<3; j++){
+}
+}
+ cin >>a[i][j];
+cout<<"Enter the values of C"<<endl;
+for(i=0; i<3; i++){
+    cout<<endl;
+    for(j=0; j<3 ; j++){
+}
+}
+ cin>> a[i][j];
+ c[i][j]=  a[i][j]+b[i][j];
+ cout<<"The sum of matrix a +matrix b ="<<c[i][j]<<endl;*/
+
+ int ax;
+ int bx;
+int c;
+float result;
+
+cout<<"Enter the value of a"<<endl;
+cin>a;
+cout<<"Enter the value of B"<<endl;
+cin>b;
+cout<<"Enter the value of C"<<endl
+cin>c;
+
+result =ax*ax+bx+c;
+cout<<"The*/
+
+ int arbic,calculs,ethics,ID;
+ int Total,Average,Grate;
+ cout<<"Welcome to the ULK "<<endl;
+ cout<<"------------------------------------------------------------------------------------------"<<endl;
+ cout<<"Enter the rule number of first student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<"A"<<endl;
+ }else  if(arbic>10 && arbic>15){
+    cout<<"The grate of arbic is B"<<endl;
+  }else if(arbic<5 && arbic>9){
+    cout<<" The grate of arbic is C"<<endl;
+  }else if(arbic>5){
+  cout<<"The grate of arbic is Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<" The grate of calculs is A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs is B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<" The grate of calculs is C"<<endl;
+  }else if(calculs>5){
+  cout<<" The grate of calculs is Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"The grate of ethics is A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<"The grate of ethics is B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<"The grate of ethics is C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics is Fail"<<endl;
+ }
+cout<<"-----------------------------------------------------------------------------------------------------"<<endl;
+ cout<<"Enter the rule number of secod student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<"The grate of arbic is A"<<endl;
+ }else  if(arbic>=10 && arbic>=15){
+    cout<<" The grate of arbic is B"<<endl;
+  }else if(arbic<=5 && arbic>=9){
+    cout<<" The grate of arbic is C"<<endl;
+  }else if(arbic>5){
+  cout<<" The grate of arbic is Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<"The grate of calculs is A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs is B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<" The grate of calculs is C"<<endl;
+  }else if(calculs>5){
+  cout<<"The grate of calculs is Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"The grate of ethics is A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<" The grate of ethics is B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<" The grate of ethics is C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics is Fail"<<endl;
+ }
+
+cout<<"----------------------------------------------------------------------------------------------------------"<<endl;
+
+   cout<<"Enter the rule number of thied student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<" The grate of arbic is A"<<endl;
+ }else  if(arbic>=10 && arbic>=15){
+    cout<<" The grate of arbic is B"<<endl;
+  }else if(arbic<=5 && arbic>=9){
+    cout<<" The grate of arbic is C"<<endl;
+  }else if(arbic>5){
+  cout<<"The grate of arbic is Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<"A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs is B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<" The grate of calculs is C"<<endl;
+  }else if(calculs>5){
+  cout<<"The grate of calculs is Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<"The grate of ethics is B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<"The grate of ethics is C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics is Fail"<<endl;
+ }
+
+cout<<"--------------------------------------------------------------------------------------------------------------------"<<endl;
+
+  cout<<"Enter the rule number of four student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<"The grate of arbic is A"<<endl;
+ }else  if(arbic>=10 && arbic>=15){
+    cout<<"The grate of arbic is B"<<endl;
+  }else if(arbic<=5 && arbic>=9){
+    cout<<"The grate of arbic is C"<<endl;
+  }else if(arbic>5){
+  cout<<"The grate of arbic is Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<"The grate of calculs is A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs is B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<"The grate of calculs is C"<<endl;
+  }else if(calculs>5){
+  cout<<"The grate of calculs is Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"The grate of ethics is A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<"The grate of ethics is B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<"The grate of ethics is C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics is Fail"<<endl;
+ }
+
+cout<<"-----------------------------------------------------------------------------------------------------------------"<<endl;
+
+  cout<<"Enter the rule number of five student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<"The grate of arbic is A"<<endl;
+ }else  if(arbic>=10 && arbic>=15){
+    cout<<" The grate of arbic B"<<endl;
+  }else if(arbic<=5 && arbic>=9){
+    cout<<"The grate of arbic  C"<<endl;
+  }else if(arbic>5){
+  cout<<"The grate of arbic  Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<"The grate of calculs A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<"The grate of calculs C"<<endl;
+  }else if(calculs>5){
+  cout<<"The grate of calculs Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"The grate of ethics A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<"The grate of ethics B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<"The grate of ethics C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics Fail"<<endl;
+ }
+
+ cout<<"---------------------------------------------------------------------------------------------------------------"<<endl;
+
+  cout<<"Enter the rule number of six student"<<endl;
+ cin>>ID;
+ cout<<"Enter the marks of arbic"<<endl;
+ cin>>arbic;
+
+ cout<<"Enter the marks of calculs"<<endl;
+ cin>>calculs;
+
+
+ cout<<"Enter the marks of ethics"<<endl;
+ cin>>ethics;
+
+ Total =arbic+calculs+ethics;
+ Average =Total/3;
+ cout<<"The Total is ="<<Total<<endl;
+cout<<"The  Average is ="<< Average<<endl;
+
+  if(arbic<=16 && arbic>=20){
+    cout<<"The grate of arbic A"<<endl;
+ }else  if(arbic>=10 && arbic>=15){
+    cout<<"The grate of arbic B"<<endl;
+  }else if(arbic<=5 && arbic>=9){
+    cout<<"The grate of arbic C"<<endl;
+  }else if(arbic>5){
+  cout<<"The grate of arbic Fail"<<endl;
+  }
+
+    if(calculs<=16 && calculs>=20){
+    cout<<"The grate of calculs A"<<endl;
+ }else  if(calculs<=10 && calculs>=15){
+    cout<<"The grate of calculs B"<<endl;
+  }else if(calculs<=5 && calculs>=9){
+    cout<<"The grate of calculs C"<<endl;
+  }else if(calculs>5){
+  cout<<"The grate of calculs Fail"<<endl;
+  }
+
+    if(ethics<=16 && ethics>=20){
+    cout<<"The grate of ethics A"<<endl;
+ }else  if(ethics<=10 && ethics>=15){
+    cout<<"The grate of ethics B"<<endl;
+  }else if(ethics<=5 && ethics>=9){
+    cout<<"The grate of ethics C"<<endl;
+  }else if(ethics>5){
+       cout<<"The grate of ethics Fail"<<endl;
+ }
+
+return 0;
+}
